ba2b76671b585fd3317974a12d3578cf99697f50
Khalildubruh <fortechsignup@gmail.com>
2019-05-21 10:53:39 -0700

    I started work on Homescreen
    I started to work on the mnu and the homescreen to be able to to have a start button.

---
+++ Game-Project/MenuHomescreen/sketch.js
@@ -0,0 +1,19 @@
+<body onload = "initialize()">
+<button type = "button"
+id = "startBtn"
+oneclick = "initialize()"> START</button>
+
+
+function setup (){
+createCanvas(1000,1000)
+
+startBtn {
+position: absolute;
+top: 400px;
+left: 250px;
+width: 200px;
+height 50px;
+font-size: 20px;
+
+
+}
